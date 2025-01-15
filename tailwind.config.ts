eb7545ff12f778d90ea3fa68504f5eba4454e115
markthos <aydenthos@gmail.com>
2025-01-14 16:13:56 -0800

    finished heading and body

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import fluid, { extract } from 'fluid-tailwind';
+import fluid, { extract, screens, fontSize, FluidThemeConfig } from 'fluid-tailwind';
 
 export default {
   content: {
@@ -11,7 +11,20 @@ export default {
     ],
     extract},
   theme: {
+    fluid: (({ theme }) => ({
+      defaultScreens: ['20rem', theme('screens.lg')]
+    })) satisfies FluidThemeConfig,
+    screens, fontSize,
     extend: {
+      colors: {
+        "brand-blue": "#4876ff",
+        "brand-lime": "#d9f154",
+        "brand-navy": "#2e3192",
+        "brand-orange": "#ff7347",
+        "brand-pink": "#f7d0e9",
+        "brand-purple": "#692e54",
+        "brand-gray": "#fffdf9",
+      },
       fontFamily: {
         sans: ['var(--font-bowlby-sc)'],
         mono: ['var(--font-dm-mono)'],
