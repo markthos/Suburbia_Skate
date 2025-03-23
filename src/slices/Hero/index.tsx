71e36821f207e4728a07f3e0c2da8357ffc48c3c
markthos <aydenthos@gmail.com>
2025-03-23 15:00:19 -0700

    finished setup for animations and board on Hero Slice

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,6 +1,6 @@
 
 import { asImageSrc, Content } from "@prismicio/client";
-import { PrismicText, SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
 import { JSX } from "react";
 
 import { Bounded } from "@/components/Bounded";
@@ -38,25 +38,29 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       className="bg-brand-pink relative h-dvh overflow-hidden text-zinc-800 bg-texture"
     >
       <div className="absolute inset-0 flex items-center pt-20">
-        <WideLogo className="w-full text-brand-purple hidden opacity-20 mix-blend-multiply lg:block"/>
-        <TallLogo className="w-full text-brand-purple opacity-20 mix-blend-multiply lg:hidden"/>
+        <WideLogo className="w-full text-brand-purple hidden opacity-20 mix-blend-multiply lg:block" />
+        <TallLogo className="w-full text-brand-purple opacity-20 mix-blend-multiply lg:hidden" />
       </div>
-      <div className="grid absolute inset-0 mx-auto mt-24 max-w-6xl grid-rows-[1fr,auto] place-items-end px-6 ~py-10/16">
 
-      <Heading size="lg" className="relative max-w-2x1 place-self-start">
-        <PrismicText field={slice.primary.heading} />
-      </Heading>
-      <div className="flex relative w-full flex-col items-center justify-between ~gap-2/4 lg:flex-row">
-        <div className="max-w-[45ch] font-semibold ~text-lg/x1">
-
-          <PrismicText field={slice.primary.body} />
+      <div className="absolute inset-0 mx-auto mt-24 grid max-w-6xl grid-rows-[1fr,auto] place-items-end px-6 ~py-10/16">
+        <Heading className="relative max-w-2xl place-self-start">
+          <PrismicText field={slice.primary.heading} />
+        </Heading>
+        <div className="flex relative w-full flex-col items-center justify-between ~gap-2/4 lg:flex-row">
+          <div className="max-w-[45ch] font-semibold ~text-lg/xl">
+            <PrismicRichText field={slice.primary.body} />
+          </div>
+          <ButtonLink
+            field={slice.primary.button}
+            icon="skateboard"
+            size="lg"
+            className="z-20 mt-2 block"
+          >
+            {slice.primary.button.text}
+          </ButtonLink>
         </div>
-      <ButtonLink field={slice.primary.button}
-        icon="skateboard" size="lg" className="z-20 mt-2 block">
-        {slice.primary.button.text}
-      </ButtonLink>
       </div>
-      </div>
+
       <InteractiveSkateboard
         deckTextureURL={deckTextureURL}
         wheelTextureURL={wheelTextureURL}
