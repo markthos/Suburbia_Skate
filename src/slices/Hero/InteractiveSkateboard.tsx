8e82eb511ad5cdacfc4c23d4ec21c2704912d0a2
markthos <aydenthos@gmail.com>
2025-03-30 14:31:02 -0700

    added board animation tricks and updated node and npm

--- src/slices/Hero/InteractiveSkateboard.tsx
+++ src/slices/Hero/InteractiveSkateboard.tsx
@@ -1,8 +1,11 @@
 "use client"
+
+import * as THREE from 'three'
 import { Skateboard } from '@/components/Skateboard'
 import { ContactShadows, Environment, OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React, { Suspense } from 'react'
+import { Canvas, ThreeEvent } from '@react-three/fiber'
+import React, { Suspense, useRef } from 'react'
+import gsap from 'gsap'
 
 type Props = {
   deckTextureURL: string;
@@ -39,20 +42,136 @@ function Scene({
   truckColor,
   boltColor,
 }: Props) {
+  const containerRef = useRef<THREE.Group>(null);
+  const originRef = useRef<THREE.Group>(null);
+
+  function onClick(event: ThreeEvent<MouseEvent>) {
+    event.stopPropagation();
+
+    const board = containerRef.current;
+    const origin = originRef.current;
+
+    if (!board || !origin) return;
+
+    const { name } = event.object;
+
+    if (name === "back") {
+      ollie(board);
+    } else if (name === "middle") {
+      kickflip(board);
+    } else if (name === "front") {
+      frontside360(board, origin);
+    }
+
+    function kickflip(board: THREE.Group) {
+      jumpBoard(board);
+  
+      gsap
+        .timeline()
+        .to(board.rotation, { x: -0.6, duration: 0.26, ease: "none" })
+        .to(board.rotation, { x: 0.4, duration: 0.82, ease: "power2.in" })
+        .to(
+          board.rotation,
+          {
+            z: `+=${Math.PI * 2}`,
+            duration: 0.78,
+            ease: "none",
+          },
+          0.3
+        )
+        .to(board.rotation, { x: 0, duration: 0.12, ease: "none" });
+    }
+
+    function ollie(board: THREE.Group) {
+      jumpBoard(board);
+
+      gsap.timeline()
+      .to(board.rotation, {
+        x: -.6,
+        duration: .26,
+        ease: 'none',
+      })
+      .to(board.rotation, {
+        x: .4,
+        duration: .82,
+        ease: 'power2.in',
+      })
+      .to(board.rotation, {
+        x: 0,
+        duration: .26,
+        ease: 'none',
+      });
+      }
+      
+    function frontside360(board: THREE.Group, origin: THREE.Group) {
+      jumpBoard(board);
+
+      gsap
+        .timeline()
+        .to(board.rotation, { x: -0.6, duration: 0.26, ease: "none" })
+        .to(board.rotation, { x: 0.4, duration: 0.82, ease: "power2.in" })
+        .to(
+          origin.rotation,
+          {
+            y: `+=${Math.PI * 2}`,
+            duration: 0.77,
+            ease: "none",
+          },
+          0.3
+        )
+        .to(board.rotation, { x: 0, duration: 0.14, ease: "none" });
+    }
+    
+
+
+    function jumpBoard(board: THREE.Group) {
+      gsap.timeline()
+      .to(board.position, {
+        y: .8,
+        duration: .51,
+        ease: 'power2.Out',
+        delay: .26,
+      })
+      .to(board.position, {
+        y: 0,
+        duration: .43,
+        ease: 'power2.In',
+      });
+      }
+  }
+    
     return (
         <group>
           <OrbitControls />
-          <Environment files={"/hdr/warehouse-256.hdr"} />
-          <Skateboard
-              wheelTextureURLs={[wheelTextureURL]}
-              wheelTextureURL={wheelTextureURL}
-              deckTextureURLs={[deckTextureURL]}
-              deckTextureURL={deckTextureURL}
-              truckColor={truckColor}
-              boltColor={boltColor}
-              constantWheelSpin
-          />
-        <ContactShadows opacity={0.6} position={[0, -0.08, 0]} />
+        <Environment files={"/hdr/warehouse-256.hdr"} />
+          <group ref={originRef}>
+            <group ref={containerRef} position={[-.25, 0, -.635]}>
+              <group position={[0, -0.086, .635]}>
+                <Skateboard
+                    wheelTextureURLs={[wheelTextureURL]}
+                    wheelTextureURL={wheelTextureURL}
+                    deckTextureURLs={[deckTextureURL]}
+                    deckTextureURL={deckTextureURL}
+                    truckColor={truckColor}
+                    boltColor={boltColor}
+                    constantWheelSpin
+                />
+                <mesh position = {[0, 0.27, 0.9]} name='front' onClick={onClick}>
+                  <boxGeometry args={[0.6, 0.2, .58]} />
+                  <meshStandardMaterial visible={false} />
+                </mesh>
+                <mesh position = {[0, 0.27, 0]} name='middle' onClick={onClick}>
+                  <boxGeometry args={[0.6, 0.1, 1.2]} />
+                  <meshStandardMaterial visible={false} />
+                </mesh>
+                <mesh position = {[0, 0.27, -0.9]} name='back' onClick={onClick}>
+                  <boxGeometry args={[0.6, 0.2, .58]} />
+                  <meshStandardMaterial visible={false} />
+                </mesh>
+              </group>
+            </group>
+          </group>
+          <ContactShadows opacity={0.6} position={[0, -0.08, 0]} />
         </group>
     )
 }
