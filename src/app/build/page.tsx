4c5d6e47ec32520b6de58cdd6557ab676478b786
markthos <aydenthos@gmail.com>
2025-03-31 15:47:37 -0700

    finalizing and added loading screen

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -9,6 +9,7 @@ import { createClient } from "@/prismicio";
 import Preview from './preview'
 import { asImageSrc } from '@prismicio/client'
 import Controls from './controls'
+import Loading from './loading'
 
 type SearchParams = {
     wheel?: string;
@@ -45,39 +46,40 @@ export default async function page(
 
   return (
       <div className="flex min-h-screen flex-col lg:flex-row">
-        <CustomizerControlsProvider
-            defaultWheel={defaultWheel}
-            defaultDeck={defaultDeck}
-            defaultTruck={defaultTruck}
-            defaultBolt={defaultBolt}
-        >
-              <div className="relative aspect-square shrink-0 bg-[#3a414a] lg:aspect-auto lg:grow">
-                <div className="absolute inset-0">
-                    <Preview
-                    deckTextureURLs={deckTextureURLs}
-                    wheelTextureURLs={wheelTextureURLs}
-                    />
+              <CustomizerControlsProvider
+                defaultWheel={defaultWheel}
+                defaultDeck={defaultDeck}
+                defaultTruck={defaultTruck}
+                defaultBolt={defaultBolt}
+            >
+                <div className="relative aspect-square shrink-0 bg-[#3a414a] lg:aspect-auto lg:grow">
+                    <div className="absolute inset-0">
+                        <Preview
+                        deckTextureURLs={deckTextureURLs}
+                        wheelTextureURLs={wheelTextureURLs}
+                        />
+                    </div>
+                    <Link href="/" className="absolute top-6 left-6">
+                        <Logo className="h-12 text-white" />
+                    </Link>
                 </div>
-                <Link href="/" className="absolute top-6 left-6">
-                    <Logo className="h-12 text-white" />
-                </Link>
-            </div>
-            <div className="grow bg-texture bg-zinc-900 text-white ~p-4/6 lg:w-96 lg:shrink-0 lg:grow-0">
-                <Heading as="h1" size="sm" className="mb-6 mt-0">
-                    Build your board 
-                </Heading>
-                  <Controls
-                      wheels={wheels}
-                      decks={decks}
-                      metals={metals}
-                      className="mb-6"
-                      
-                  />
-                <ButtonLink href="" color="lime" icon="plus">
-                    Add to Cart
-                </ButtonLink>
-            </div>
-          </CustomizerControlsProvider>
+                <div className="grow bg-texture bg-zinc-900 text-white ~p-4/6 lg:w-96 lg:shrink-0 lg:grow-0">
+                    <Heading as="h1" size="sm" className="mb-6 mt-0">
+                        Build your board 
+                    </Heading>
+                    <Controls
+                        wheels={wheels}
+                        decks={decks}
+                        metals={metals}
+                        className="mb-6"
+                        
+                    />
+                    <ButtonLink href="" color="lime" icon="plus">
+                        Add to Cart
+                    </ButtonLink>
+                </div>
+            </CustomizerControlsProvider>
+          <Loading />
     </div>
   )
 }
